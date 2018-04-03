c9babf3bfac429999a28ced62370ec679574cc82
KDBDEV <ken.brantley.ccat@gmail.com>
2018-04-03 16:38:28 -0400

    installing npm modules

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
-/*import subtract, { square, add } from './utils.js';
-console.log("app.js is running!!");
-console.log(square(4));
-console.log(add(2,3));
-console.log(subtract(20,1));
-*/
- import isSenior, { isAdult, canDrink } from './person.js';
- console.log(isAdult(18));
- console.log(canDrink(21));
- console.log(isSenior(65));
+// install
+// import
+// use
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const template = React.createElement('p',{},'testing');
+ReactDOM.render(template,document.getElementById('app'));
