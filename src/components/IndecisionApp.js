b59d10b1dbfb8d914f65a326378ea01e121278ca
KDBDEV <ken.brantley.ccat@gmail.com>
2018-04-04 14:05:59 -0400

    es6 class props refactor

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -4,15 +4,53 @@ import Action from './Action.js';
 import Header from './Header.js';
 import Options from './Options.js';
 export default class IndecisionApp extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-        this.handlePick = this.handlePick.bind(this);
-        this.handleAddOption = this.handleAddOption.bind(this);
-        this.handleDeleteOption = this.handleDeleteOption.bind(this);
-        this.state = {
-            options: []
-        };
+    state = {
+        options: []
+    };
+    handleDeleteOptions = () => {
+        this.setState(() => ({options: [] }));
+    };
+    handleDeleteOption = (optionToRemove) => {
+        this.setState((prevState) => ({
+            options: prevState.options.filter((option)=> optionToRemove !== option)
+        }));
+    };
+    handlePick = () => {
+        this.setState(() => {
+            const randomNum = Math.floor(Math.random() * this.state.options.length);
+            const option = this.state.options[randomNum];
+            alert(option)
+        });
+    };
+    handleAddOption = (option) => {
+        if (!option) {
+            return 'Enter valid value to add item';
+        } else if(this.state.options.indexOf(option) > -1) {
+            return 'This option already exists';
+        }
+        this.setState((prevState) => ({
+            options: prevState.options.concat(option)
+        }));
+    };
+    render() {
+        const subtitle = "Put your life in the hands of a computer";
+        return (
+            <div>
+                <Header subtitle = {subtitle}/>
+                <Action 
+                hasOptions={this.state.options.length > 0}
+                handlePick={this.handlePick}
+                />
+                <Options 
+                options = {this.state.options}
+                handleDeleteOptions={this.handleDeleteOptions}
+                handleDeleteOption = {this.handleDeleteOption}
+                />
+                <AddOption
+                handleAddOption ={this.handleAddOption}
+                />
+            </div>
+        );
     }
     componentDidMount() {
         try {
@@ -33,50 +71,5 @@ export default class IndecisionApp extends React.Component {
     }
     componentWillUnmount() {
         console.log('component will unmount');
-    }
-    handleDeleteOptions() {
-        this.setState(() => ({options: [] }));
-    }
-    handleDeleteOption (optionToRemove) {
-        this.setState((prevState) => ({
-            options: prevState.options.filter((option)=> optionToRemove !== option)
-        }));
-    }
-    handlePick() {
-        this.setState(() => {
-            const randomNum = Math.floor(Math.random() * this.state.options.length);
-            const option = this.state.options[randomNum];
-            alert(option)
-        });
-    }
-    handleAddOption(option) {
-        if (!option) {
-            return 'Enter valid value to add item';
-        } else if(this.state.options.indexOf(option) > -1) {
-            return 'This option already exists';
-        }
-        this.setState((prevState) => ({
-            options: prevState.options.concat(option)
-        }));
-    }
-    render() {
-        const subtitle = "Put your life in the hands of a computer";
-        return (
-            <div>
-                <Header subtitle = {subtitle}/>
-                <Action 
-                hasOptions={this.state.options.length > 0}
-                handlePick={this.handlePick}
-                />
-                <Options 
-                options = {this.state.options}
-                handleDeleteOptions={this.handleDeleteOptions}
-                handleDeleteOption = {this.handleDeleteOption}
-                />
-                <AddOption
-                handleAddOption ={this.handleAddOption}
-                />
-            </div>
-        );
     }
 }
